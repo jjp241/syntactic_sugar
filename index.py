01da2034dfeb77227ee11c975002fae25c1ec73d
Jakub Podolak <jp429585@students.mimuw.edu.pl>
2022-11-26 00:43:58 +0100

    Added first version of token authentication

--- index.py
+++ index.py
@@ -4,10 +4,13 @@ from flask import Flask,\
                   request,\
                   flash,\
                   redirect
+import time
 
+from lib.token_authentication import is_token_file_present, get_goldman_token_value
 
 app = Flask(__name__, static_url_path='')
 app.config['SECRET_KEY'] = "2b3f12f3ef12a6c86b"
+
 
 # What to do, when we receive GET or POST at index
 @app.route('/', methods=['GET', 'POST'])
@@ -25,6 +28,18 @@ def test1():
 @app.route('/test2', methods=['GET'])
 def test2():
    return render_template('test2.html')
+
+@app.route('/forward', methods=['GET', 'POST'])
+def forward():
+   print('here')
+   if is_token_file_present():
+      print('token present')
+      token = get_goldman_token_value()
+      print('token')
+      return render_template('test2.html', token=token)
+   else:
+      return redirect('/')
+
 
 
 if __name__ == "__main__":
